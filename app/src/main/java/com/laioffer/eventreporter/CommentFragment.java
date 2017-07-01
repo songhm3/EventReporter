f6163654448a04d3112c8962b8f568ad4fffb93f
songhm3 <haimengs@usc.edu>
2017-07-01 10:55:44 +0800

    initial commit

---
+++ app/src/main/java/com/laioffer/eventreporter/CommentFragment.java
@@ -0,0 +1,74 @@
+package com.laioffer.eventreporter;
+
+
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CommentFragment extends Fragment {
+
+    GridView gridView;
+
+    OnItemSelectListener mCallback;
+
+
+    public CommentFragment() {
+        // Required empty public constructor
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_comment, container, false);
+        gridView = (GridView) view.findViewById(R.id.comment_grid);
+        gridView.setAdapter(new EventAdapter(getActivity()));
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                mCallback.onCommentSelected(i);
+            }
+        });
+
+        return view;
+    }
+
+    public void onItemSelected(int position) {
+        for (int i = 0; i < gridView.getChildCount(); i++){
+            if (position == i) {
+                gridView.getChildAt(i).setBackgroundColor(Color.BLUE);
+            } else {
+                gridView.getChildAt(i).setBackgroundColor(Color.parseColor("#EEEEEE"));
+            }
+        }
+
+    }
+
+    //Container Activity must implement this interface
+
+    public interface OnItemSelectListener {
+        public void onCommentSelected(int position);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            mCallback = (OnItemSelectListener) context;
+        } catch (ClassCastException e) {
+            //do something
+        }
+    }
+}
---
+++ app/src/main/java/com/laioffer/eventreporter/ReportEventFragment.java
@@ -0,0 +1,138 @@
+package com.laioffer.eventreporter;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class ReportEventFragment extends Fragment {
+    private final static String TAG = ReportEventFragment.class.getSimpleName();
+    private EditText mTextViewLocation;
+    private EditText getmTextViewDest;
+    private Button mReportButton;
+    private DatabaseReference database;
+    private String username;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    public ReportEventFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_report_event, container, false);
+        mTextViewLocation = (EditText) view.findViewById(R.id.text_event_location);
+
+
+        getmTextViewDest = (EditText) view.findViewById(R.id.text_event_description);
+        mReportButton = (Button) view.findViewById(R.id.button_report);
+        username = ((EventActivity)getActivity()).getUsername();
+        database = FirebaseDatabase.getInstance().getReference();
+
+
+
+        mReportButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String key = uploadEvent();
+            }
+        });
+
+        //auth
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+            }
+        };
+
+        mAuth.signInAnonymously().addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                Log.d(TAG, "signInAnonymously:onComplete:" + task.isSuccessful());
+                if (!task.isSuccessful()) {
+                    Log.w(TAG, "signInAnonymously", task.getException());
+                }
+            }
+        });
+
+
+
+        return view;
+    }
+
+    /**
+     * upload data and set path
+     */
+    private String uploadEvent() {
+        String location = mTextViewLocation.getText().toString();
+        String description = getmTextViewDest.getText().toString();
+        if (location.equals("") || description.equals("")) {
+            return "";
+        }
+        //create event instance
+        Event event = new Event();
+        event.setLocation(location);
+        event.setDescription(description);
+        event.setTime(System.currentTimeMillis());
+        event.setUser(username);
+        String key = database.child("events").push().getKey();
+        event.setId(key);
+        database.child("events").child(key).setValue(event, new DatabaseReference.CompletionListener() {
+            @Override
+            public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+                if (databaseError != null) {
+                    Toast toast = Toast.makeText(getContext(), "The event is failed, please check you network status.", Toast.LENGTH_SHORT);
+                    toast.show();
+                } else {
+                    Toast toast = Toast.makeText(getContext(), "The event is reported", Toast.LENGTH_SHORT);
+                    toast.show();
+                    mTextViewLocation.setText("");
+                    getmTextViewDest.setText("");
+                }
+            }
+        });
+        return key;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+
+}
